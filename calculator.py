40cc9a8761e76bee9c176232047dcce6f10f2077
Rei <75221077+hyperintelligent@users.noreply.github.com>
2021-02-10 17:01:20 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,6 @@
+print("BTC Calculator made by Rei#1111/hyperintelligent")
+factor = int(input("how many BTC do you have? :"))
+
+math = factor * 40000
+
+print("You can make about " + str(math) + " if you sell right now!")
